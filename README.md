d5cc7c79ff0c37850cb4617b17dddedf009f5c3a
Dave Irvine <dave@daveirvine.me>
2020-11-27 11:24:26 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# amazon-ecs-register-task-definition
---
+++ index.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const core = require('@actions/core');
+const aws = require('aws-sdk');
+const yaml = require('yaml');
+const fs = require('fs');
+
+// Attributes that are returned by DescribeTaskDefinition, but are not valid RegisterTaskDefinition inputs
+const IGNORED_TASK_DEFINITION_ATTRIBUTES = [
+    'compatibilities',
+    'taskDefinitionArn',
+    'requiresAttributes',
+    'revision',
+    'status'
+];
+
+function isEmptyValue(value) {
+    if (value === null || value === undefined || value === '') {
+        return true;
+    }
+
+    if (Array.isArray(value)) {
+        for (var element of value) {
+            if (!isEmptyValue(element)) {
+                // the array has at least one non-empty element
+                return false;
+            }
+        }
+        // the array has no non-empty elements
+        return true;
+    }
+
+    if (typeof value === 'object') {
+        for (var childValue of Object.values(value)) {
+            if (!isEmptyValue(childValue)) {
+                // the object has at least one non-empty property
+                return false;
+            }
+        }
+        // the object has no non-empty property
+        return true;
+    }
+
+    return false;
+}
+
+function emptyValueReplacer(_, value) {
+    if (isEmptyValue(value)) {
+        return undefined;
+    }
+
+    if (Array.isArray(value)) {
+        return value.filter(e => !isEmptyValue(e));
+    }
+
+    return value;
+}
+
+function cleanNullKeys(obj) {
+    return JSON.parse(JSON.stringify(obj, emptyValueReplacer));
+}
+
+function removeIgnoredAttributes(taskDef) {
+    for (var attribute of IGNORED_TASK_DEFINITION_ATTRIBUTES) {
+        if (taskDef[attribute]) {
+            core.warning(`Ignoring property '${attribute}' in the task definition file. ` +
+                'This property is returned by the Amazon ECS DescribeTaskDefinition API and may be shown in the ECS console, ' +
+                'but it is not a valid field when registering a new task definition. ' +
+                'This field can be safely removed from your task definition file.');
+            delete taskDef[attribute];
+        }
+    }
+
+    return taskDef;
+}
+
+async function run() {
+    try {
+        const ecs = new aws.ECS({
+            customUserAgent: 'amazon-ecs-register-task-definition-for-github-actions'
+        });
+
+        const taskDefinitionFile = core.getInput('task-definition', { required: true });
+
+        // Register the task definition
+        core.debug('Registering the task definition');
+        const taskDefPath = path.isAbsolute(taskDefinitionFile) ?
+            taskDefinitionFile :
+            path.join(process.env.GITHUB_WORKSPACE, taskDefinitionFile);
+        const fileContents = fs.readFileSync(taskDefPath, 'utf8');
+        const taskDefContents = removeIgnoredAttributes(cleanNullKeys(yaml.parse(fileContents)));
+
+        let registerResponse;
+        try {
+            registerResponse = await ecs.registerTaskDefinition(taskDefContents).promise();
+        } catch (error) {
+            core.setFailed("Failed to register task definition in ECS: " + error.message);
+            core.debug("Task definition contents:");
+            core.debug(JSON.stringify(taskDefContents, undefined, 4));
+            throw (error);
+        }
+
+        const taskDefArn = registerResponse.taskDefinition.taskDefinitionArn;
+        core.setOutput('task-definition-arn', taskDefArn);
+    }
+    catch (error) {
+        core.setFailed(error.message);
+        core.debug(error.stack);
+    }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+    run();
+}
